63193d52e6abcaa0506eb5e34d496312adb86759
Mika Anttila <mika.anttila@tuni.fi>
2020-08-10 20:57:21 +0300

    We'll give also REQUEST_URI to script

--- cmdline-cgitest.sh
+++ cmdline-cgitest.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-HTTP_USER_AGENT=cmdline QUERY_STRING="${1:-date=20190910}" ./parse.py
+HTTP_USER_AGENT=cmdline QUERY_STRING="${1:-date=20190910}" REQUEST_URI="${0}${QUERY_STRING}" ./parse.py
