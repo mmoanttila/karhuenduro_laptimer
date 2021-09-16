697b37619ce98658415c083ebd425e76a99aa3b1
Mika Anttila <mmoa-github@mmoa.net>
2021-09-16 16:29:38 +0300

    Changed tag filter from BAD0 to 0000

--- parse.py
+++ parse.py
@@ -19,7 +19,7 @@ startports = [1, 3]
 endports = [2, 4]
 log_url = "https://www.karhuenduro.fi/ajanotto/"
 csv_url = "https://docs.google.com/spreadsheets/d/1dtqQQ6azJ5J0VBEHnnKLAkp3SlUK_6OzSk2RQivY6L0/export?format=csv&id=1dtqQQ6azJ5J0VBEHnnKLAkp3SlUK_6OzSk2RQivY6L0&gid=0"
-tag_filter = "^BAD0...."
+tag_filter = "^0000...."
 log_dir = "../web/ajanotto/"
 output_dir = "../web/tulokset/"
 output_file_name = ""
