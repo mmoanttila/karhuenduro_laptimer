adc1d8452588c356f75fbacaacda7bc6291cd84c
Mika Anttila <mmoa@iki.fi>
2023-09-12 22:05:39 +0300

    Some merging

--- parse.py
+++ parse.py
@@ -31,6 +31,7 @@ static_list = ""
 sarjakrossit_numbers = "../web/ilmo/ilmot/sarjakrossit.csv"
 kanada_numbers = "../web/ilmo/ilmot/kanada.csv"
 
+pisteet = [25, 22, 20, 18, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 # End of settings
 
 # Parameters:
